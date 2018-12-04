894dedb8639b08d295a8f5e7a5541578055b30a3
Suraj Jena <jena.suraj.k@gmail.com>
2018-12-03 21:04:53 -0700

    insert() function implementation added - untested.

--- src/adt.rs
+++ src/adt.rs
@@ -1,6 +1,6 @@
 pub extern crate holochain_core_types;
 pub extern crate holochain_wasm_utils;
-pub use hdk::api::*;
+pub use hdk::*;
 pub use entries::*;
 
 use std::ptr;
@@ -11,22 +11,20 @@ use std::ptr;
 ///         - Ok(()) if creation of the root node succeeded.
 ///         - Err(()) if failure for any reason.
 ///
-pub fn Trie(type: str, bucketing: bool) -> Result<()>
+pub fn Trie(type: str, bucketing: bool) -> Result<JsonString, String>
 {
     let root_node: Trie;
     root_node.data = type;
     root_node.bucketing = bucketing;
-    let commit_result = commit_entry(&root_node);
-    match commit_result
+    match commit_entry(&root_node)
     {
-        Ok(v) => { v },
+        Ok(address) => Ok(address.into()),
         Err(e) =>
         {
-            println!("Error in Trie initialization: {:?}");
-            Err(());
+            println!("Error in Trie initialization: {:?}.", e);
+            Err(e.into());
         },
     }
-    Ok(());
 }
 
 /// The insert() function for each word.
@@ -35,9 +33,70 @@ pub fn Trie(type: str, bucketing: bool) -> Result<()>
 ///     - Also adds link from anchor of type `category` to root of string.
 ///       anchorText is `string` itself, or `id` if provided.
 ///
-pub fn insert(data: str, category: Option<str>, id: Option<i32>) -> Result<()>
+pub fn insert(name: Address, data: String, category: Option<str>, id: Option<i32>) -> Result<JsonString, String>
 {
+    match get_entry(name)
+    {
+        Ok(t) => {
+            match t
+            {
+                Some(Trie) => { println!("Trie found, proceeding to insert."); },
+                None => { return Err("No such Trie exists"); },
+            }
+        },
+        Err(e) => { panic!("Issue with get_entry() with finding root with error: {:?}.", e) },
+    }
+    let levelpeg: i32 = 0;
+    let traverser: Address = name;
 
+    for i in 0..data.len()
+    {
+        match get_links(&traverser, data[i])
+        {
+            Ok(t) => {
+                traverser = t[0];
+                continue;
+            },
+            Err(e) => {
+                levelpeg = i;
+            },
+        }
+    }
+
+    for j in levelpeg..data.len() + 1
+    {
+        let node = TrieNode {
+            data: data[j],
+            level: j,
+        };
+        if j == data.len() + 1
+        {
+            node.data = "\0";
+            node.level = -2;
+        }
+        match commit_entry(&node)
+        {
+            Ok(address) => {
+                if j == data.len() + 1
+                {
+                    match link_entries(&traverser, &address, data)
+                    {
+                        Ok(t) => { return Ok(address); },
+                        Err(e) => { return Err("Linking Null failed in insert with error {:?}.", e); },
+                    }
+                }
+                match link_entries(&traverser, &address, data[j])
+                {
+                    Ok(t) => {
+                        traverser = address;
+                        continue;
+                    },
+                    Err(e) => { return Err("Linking nodes failed in insert with error {:?}.", e); },
+                }
+            },
+            Err(e) => { return Err("Committing node failed with error {:?}.", e) },
+        }
+    }
 }
 
 /// The drop() function for each word - soft delete.
